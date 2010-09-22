69844c4facca041382aa7e9daaaf7a32e60be3ad
mahatma@eu.by <mahatma@eu.by@a952c524-1615-11de-9433-ad2e3fad96c5>
2010-09-22 18:46:47 +0000

    genpnprd - scripts syntax simplifying

--- sys-kernel/genpnprd/files/etc/modprobe.sh
+++ sys-kernel/genpnprd/files/etc/modprobe.sh
@@ -5,7 +5,18 @@ modparam(){ return;}
 . /etc/modparam.sh
 . ./modules.alias.sh
 
-_modprobe(){
+modprobe(){
+local r=1 INSMOD="" a=false
+while true; do
+case "$1" in
+--)shift;break;;
+-*a*)a=true;;
+-*);;
+*)break;;
+esac
+shift
+done
+$a && set "$*"
 local m="$(echo -ne "$1" | sed -e s/-/_/g)" i
 shift
 for m in $m; do
@@ -16,22 +27,9 @@ for m in $m; do
 		r=$?
 	done
 done
-}
-
-modprobe(){
-local r=1 INSMOD=""
-while true; do
-case "$1" in
--a|--all)
-	shift
-	_modprobe "$*"
-	return $r
-;;
--*);;
-*)break;;
-esac
-shift
-done
-_modprobe "${@}"
 return $r
 }
+
+case $0 in
+*modprobe*)modprobe "${@}";exit $?;;
+esac
