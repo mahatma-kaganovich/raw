5e42ba3848594182972336270aa72a3a8902c38b
mahatma <mahatma@eu.by>
2015-03-17 22:31:43 +0300

    rare fix

--- profiles/targets/rare/opengl/scan.sh
+++ profiles/targets/rare/opengl/scan.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-# run as active profile with config "-mesa -opengl -osmesa -evdev"
+# run as active profile
 
 rm -f err* package.* partial*
 
 echo "# rare mesa/opengl profile" >>package.mask
 
-FL_MASK="opengl egl osmesa evdev"
+FL_MASK_GOOD="evdev"
+FL_MASK_BAD="opengl egl osmesa"
 V0='virtual/opengl|media-libs/mesa|app-admin/eselect-opengl|virtual/glu'
-V1='[>=]virtual/opengl-7.0-r[0-9]|[^<]=?media-libs/mesa-[89]|[^<]=?app-admin/eselect-opengl-1\.([3-9]|[0-9]{2})|[^<]=virtual/glu-9.0-r1'
+V1='[>=]virtual/opengl-7.0-r[0-9]|[^<]=?media-libs/mesa-[89]|[^<]=?app-admin/eselect-opengl-1\.([3-9]|[0-9]{2})|[^<]=virtual/glu-9.0-r1|x11-libs/gtkglext'
 FIND='opengl|mesa|glu'
 NOMASK='media-libs/gst-plugins-bad|media-video/ffmpeg'
 
 PORTDIR=/usr/portage
+
+FL_MASK_OFF=" $FL_MASK_BAD $FL_MASK_GOOD"
+FL_MASK_OFF="${FL_MASK_OFF// / -}"
 
 meta(){
 	local x=`grep -h "^$2=" $PORTDIR/metadata/md5-cache/${1%%:} |sed -e "s:^$2=::" -e 's: :\n:g'|sed -e 's:^\+::'|sort -u`
@@ -43,17 +47,34 @@ _slot(){
 mask(){
 	act=mask
 	pkg="$1"
+	masked=true
 }
 
 unmask(){
 	act=unmask
 	pkg="$1"
+	unmasked=true
 }
 
 msk(){
-	([ "$2" == mask ] && (echo " $1"|grep -Pq "$NOMASK")) ||
-		echo "$1" >>package.$2
+	if [ "$2" = mask ]; then
+		(echo " $1"|grep -Pq "$NOMASK") || echo "$1" >>package.mask
+	else
+		echo "$1" >>package.unmask
+	fi
 	[ -n "$fl_mask" ] && echo "$1$fl_mask" >>package.use.$2
+}
+
+_dep(){
+	local x=" $3 " j
+	for j in $(grep -h "^[A-Z]*DEPEND=" $PORTDIR/metadata/md5-cache/${1%%:} |sed -e "s:^[A-Z]*DEPEND=::" -e 's:([^ ]*)::g'|grep -Po "[\S]+\? \([^\(\)]*($2)[^\(\)]*\)"|sed -e 's:\?.*::' -e 's::\n:' -e 's:\!::'|sort -u); do #'
+		[ -z "${x##* $j *}" -o "$3" = '*' ] && return 0
+	done
+	return 1
+}
+
+badbaduse(){
+	! _dep "$1" "$V1" "$FL_MASK_BAD" && _dep "$1" "$V1" "$FL_BAD"
 }
 
 masks(){
@@ -61,14 +82,14 @@ masks(){
 	cnt0=0
 	cnt1=0
 	for j in $l; do
-		grep -Pql "$V1" $l && cnt1=$[cnt1+1] && continue
-		grep -Pql "$V0" $l && cnt0=$[cnt0+1] && continue
+		grep -Pq "$V1" $j && cnt1=$[cnt1+1] && continue
+		grep -Pq "$V0" $j && cnt0=$[cnt0+1] && continue
 		cnt=$[cnt+1] && continue
 	done
+	cnt2=$[cnt0+cnt1]
 	echo -n " $cnt0:$cnt1:$cnt"
-	[ $cnt0 = $cnt1 -a $cnt = 0 ] && {
-		echo "$i" >>partial
-		echo ' r1'
+	[ $cnt0 = 0 -a $cnt1 != 0 -a $cnt = 0 ] && {
+		echo -n ' r1'
 		[ -n "$fl_mask" ] && echo "$i$fl_mask" >>package.use.mask
 		return 1
 	}
@@ -90,8 +111,9 @@ masks(){
 		j="${j#$PORTDIR/}"
 		j="${j%.ebuild}"
 		j="${j//\/*\//\/}"
+		! $ok && badbaduse "$j" && ok=true
 		# ??? may be on second pass?
-		[ -z "$ok" -a "$cnt0" = 0 ] && (USE="$fl_mask" emerge -pv =$j 2>&1 | grep -Pq "$V1") && {
+		[ -z "$ok" -a "$cnt2" = 0 ] && (USE="$fl_mask" emerge -pv =$j 2>&1 | grep -Pq "$V1") && {
 			echo -n "~"
 			echo "$j" >>partial.dep
 			ok=false
@@ -138,25 +160,32 @@ masks(){
 	true
 }
 
-for i in $(cd $PORTDIR && echo */*); do
-#for i in $(cd $PORTDIR && echo */glew); do
-#for i in $(cd $PORTDIR && echo dev-qt/qtopengl); do
+iuse(){
+	local j
+	for j in "${@}"; do
+		[ -z "${iuse##* $j *}" ] && echo -n " $j"
+	done
+}
+
+param="$1"
+
+for i in $(cd $PORTDIR && echo */${param:-*}); do
 	[ -d "$PORTDIR/$i" -a "$i" != virtual/opengl -a -n "${i##distfiles/*}" ] || continue
 	pn="${i##*/}"
 	j="$PORTDIR/$i/$pn-*.ebuild"
 	l=$(find "$PORTDIR/$i" -path "$j" -type f|sort -V)
 	[ "$l" = "$j" -o -z "$l" ] && continue
 	fl_mask=
-	x="$(meta "$i-*" IUSE)"
-	for j in $FL_MASK; do
-		[ -z "${x##* $j *}" ] && fl_mask+=" $j"
-	done
+	iuse="$(meta "$i-*" IUSE)"
+	fl_mask=`iuse $FL_MASK_BAD $FL_MASK_GOOD`
+	fl_mask_bad=`iuse $FL_MASK_BAD`
 	slot=
 	use0=
 	grep -Psqv "$FIND" $l || continue
 	slots=$(meta "$i-*" SLOT)
 	[ "${slots// }" = 0 ] && slots=
 	if grep -Psq "$V1" $l; then
+		badbaduse "$i-*" && continue
 		echo -n $i
 		(! masks || [ -z "${fl_mask# }" ]) && echo '' && continue
 	elif [ -z "${fl_mask# }" ]; then
@@ -168,7 +197,7 @@ for i in $(cd $PORTDIR && echo */*); do
 		echo -n " :$slot"
 		i="${i%%:*}${slot:+:$slot}"
 		loop=false
-		if e=`emerge -pv "$i" --nodeps 2>&1`; then
+		if e=`USE="$FL_MASK_OFF" emerge -pv "$i" --nodeps 2>&1`; then
 			_slot || break
 			if tst1 $fl_mask; then
 				true
@@ -188,3 +217,5 @@ for i in $(cd $PORTDIR && echo */*); do
 	echo ''
 	continue
 done
+
+(emerge --info|grep "^USE="|grep -w opengl) && unlink package.use.force
