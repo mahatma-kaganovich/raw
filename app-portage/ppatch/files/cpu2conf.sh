b02b4629008efdba93861c64f43b480f8c19fe89
mahatma <mahatma@eu.by>
2015-08-11 13:47:50 +0300

    ppatch: improve profile cpu detection

--- app-portage/ppatch/files/cpu2conf.sh
+++ app-portage/ppatch/files/cpu2conf.sh
@@ -13,10 +13,28 @@ _f(){
 	done
 }
 
+_cmp(){
+	local i i0 i1 ok
+	if i0=`echo "$c0"|grep -F "$1"` &&  i=`echo "$c"|grep -F "$1"`; then
+		for i in $i; do
+			[ -z "${i##/*}" ] && continue
+			ok=true
+			for i1 in $i0; do
+				[ "$i" = "$i1" ] && ok=false && break
+			done
+			$ok && echo -n " $2$i"
+		done
+	fi
+}
+
+_flags(){
+	grep -s "^$1	" /proc/cpuinfo|sort -u|sed -e "s/^$1	: //" -e 's:,: :g'
+}
+
 conf_cpu(){
-local flags f0= f1= f2= f3= i j i1 c
-flags=`grep -s "^flags	" /proc/cpuinfo|sort -u`
-flags="${flags#*:}"
+local flags cpucaps f0= f1= f2= f3= i j i1 c
+flags=$(_flags flags)
+cpucaps=$(_flags cpucaps)
 f0=`_f -m{tune,cpu,arch}=native`
 f3=`_f -malign-data=cacheline -momit-leaf-frame-pointer -mtls-dialect=gnu2`
 echo "CFLAGS_NATIVE=\"$f0\""
@@ -40,24 +58,15 @@ f2="${f2# }"
 i="$f1 $f2"
 i="${i//  / }"
 [ -n "${i// }" ] && echo "CPU_FLAGS_X86=\"\$CPU_FLAGS_X86 $i\""
-i="/cc1 -quiet -v help-dummy "
 j=
-if c=`_c $f0`; then
-	if j=`echo "$c"|grep -F "$i"`; then
-		j="${j#*$i}"
-		j="${j% -quiet *}"
-		if i=`echo "$c"|grep -F "/as "`; then
-			i="${i#* -mtune=}"
-			i="${i%% *}"
-			j="$j -Wa,-mtune=$i"
-		fi
-	fi
+if c0=`_c` && c=`_c $f0`; then
+	j="$(_cmp '/cc1 -quiet -v ' '')$(_cmp '/as ' '-Wa,')"
 	i1=" $j "
-	for i in $f3; do
-		(echo "$c"|grep -q "^ *-m$1 ") && [ -n "${j## -m$i }" ] && j+=" -m$i"
+	for i in $flags $cpucaps; do
+		(echo "$c"|grep -q "^ *-m$i ") && [ -n "${i1##* -m$i *}" ] && j+=" -m$i" && i1+=" -m$i"
 	done
 fi
-echo "CFLAGS_CPU=\"${j+ $j}\""
+echo "CFLAGS_CPU=\"$j\""
 echo "CFLAGS_M=\"$f3\""
 if i=`grep "^processor[ 	]*: " /proc/cpuinfo`; then
 	i="${i##*: }"
