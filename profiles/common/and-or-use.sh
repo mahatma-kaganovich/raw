84e483347ff0c72ece1de47fadd59bb3ec799c43
mahatma <mahatma@eu.by>
2016-12-21 20:14:50 +0300

    fix _auto profile

--- profiles/common/and-or-use.sh
+++ profiles/common/and-or-use.sh
@@ -4,6 +4,7 @@
 force=.force
 d=`pwd`
 export LANG=C
+list="${@:-*/*}"
 
 pkg(){
 	local p="$1" x
@@ -47,7 +48,13 @@ done
 }
 
 ap(){
-	grep -sqxF "$1" "$2" || echo "$1" >>"$2"
+	local i s="### precise"
+	[ -e "$4" ] || echo "$s
+" >"$4"
+	i="=$1$3"
+	grep -sqxF "$i" "$4" || echo "$i" >>"$4"
+	i="$2$3"
+	grep -sqxF "$i" "$4" || sed -i -e "s:^$s\$:$i\n$s:" "$4"
 }
 
 chk6(){
@@ -84,14 +91,15 @@ mkdir -p "$d"
 cd "$d" && cd /usr/portage/metadata/md5-cache || return 1
 v=${6#+}
 [ "$v" = "$6" ] && prob=0 || prob=1
+x='\|'
 for i in $1 $v; do
-	or1+="\|$i"
+	or1+="$x$i"
 done
 for i in $2 $v; do
-	or2+="\|$i"
+	or2+="$x$i"
 done
 for i in $3 $v; do
-	or3+="\|$i"
+	or3+="$x$i"
 done
 or1="${or1#??}"
 or="$or1$or2$or3"
@@ -101,56 +109,118 @@ or3="${or3#??}"
 l1=" $2${3:+ $3}"
 l1=" $1 ${l1// / -} "
 
-l=$(grep -l " \($or\) " */*) || return 1 #"
+or1='!*\('"$or1"'\)'
+or_='!*\('"$or_"'\)'
+#ww='[^()?\*\[\]]*'
+ww='[^()?\*\[]*'
+#ww='[^()]*'
+
+re1='\(\^\^\|??\) '"($ww $or1 $ww)"
+re2='^IUSE=.*+'"$or_"
+re31="$or1? ($ww $or_ $ww)"
+re32="$or_? ($ww $or1 $ww)"
+re3='\(E=\| \)\('"$re1$x$re31$x$re32"'\)'
+
+l=$(grep -l " \($or\) " $list) || return 1 #"
 
 [ -n "$3" ] && {
-for i in `grep -l '\(\^\^\|??\) ([^()]* \('"$or1"'\) [^()]*)' $l`; do
-	grep -oh '\(\^\^\|??\) ([^()]* \('"$or1"'\) [^()]*)' "$i"|while read q; do
-		[ -z "${q##\?\?*}" ] && f=true || f=false
-		q="${q#?? \(}"
-		q="${q%\)}"
-		[ -z "$q" ] && continue
+for i in $(grep -l "^REQUIRED_US.*$re3" $l); do
+	grep -oh "$re3" "$i"|while read q; do
+		q="${q#E=}"
+		q0="${q% \( *}"
+		q="${q#* \( }"
+		q="${q% \)}"
+		[ -z "${q##*[*?\[\]]*}" -o -z "${q//[ !]/}" ] && echo "Wrong REQUIRED_USE in $i - '$q'" >&2 && continue
+		q=" $q "
+		f=true
+		if1=false
+		if2=false
 		iuse=`grep "^IUSE=" "$i"`
 		iuse=" ${iuse#IUSE=} "
 		iuse1="${iuse// [+~-]/ }"
-		q=" $q "
-		x=
-		for j in $1; do
-			x="$j $x"
+		if [ "$q0" = '??' ]; then
+			true
+			f=false
+		elif [ "$q0" = '^^' ]; then
+			true
+		else
+			q0="${q0%\?}"
+			x=" $1 "
+			if [ -z "${x##* ${q0#!} *}" ]; then
+				if1=true
+			else
+				if2=true
+				j="$q"
+				q=" $q0 "
+				q0="$j"
+				for j in $q0; do
+					[ -n "${x##* ${j#!} *}" ] && q0="${q0// $j / }"
+				done
+				continue
+			fi
+			
+			[ "$q0" = "$v" ] && ! chk6 "$4" "$5" "$v" "$1" "$2" $prob $prob && continue
+#			continue
+		fi
+
+		q="${q//  / }"
+		q="${q// !/ -}"
+		# unify both results
+		if $if1; then
+			x=" $1 "
+			q="${q// / -} "
+			q="${q// - / }"
+		else
+			# sort $q in $1 order, first - unmasked
+			x=
+			for j in $1; do x="$j $x";done
+			[ -n "$v" ] && chk6 "$4" "$5" "$v" "$1" "$2" $prob $prob && x="$v $x"
+			x=" $x"
+			for j in $x; do [ -z "${q##* $j *}" ] && q="$j ${q// $j / }"; done
+
+			# get first. according to regex - it verified
+			while [[ "$q" == ' '* ]]; do q="${q# }"; done
+			q0="${q%% *}"
+			q=" ${q#* }"
+		fi
+
+		q="${q// --/ }" # ???
+		while [[ "$q" == *'  '* ]]; do q="${q//  / }"; done
+
+		[ -z "$q0" ] && continue
+		if [[ "$q0" == '!'* ]]; then
+			q0="${q0#!}"
+			q="${q// / -}"
+			q="${q%-}"
+			q="${q// --/ }"
+		fi
+		[ -n "${x##* $q0 *}" ] && continue
+
+		r1=
+		for j in $q; do
+			[ -n "${x##* ${j#-} *}" -a -n "${l1##* -${j#-} *}" ] && r1+=" $j" && q="${q// $j / }"
 		done
-		[ -n "$v" ] && chk6 "$4" "$5" "$v" "$1" "$2" $prob $prob && x="$v $x"
-		x=" $x"
-		for j in $x; do
-			[ -z "${q##* $j *}" ] && q="$j ${q// $j / }"
-		done
-		for p in $(pkg "$i"); do
-			r=
-			r1=
-			u=
-			for q in $q; do
-				q1=
-				if [ -z "${x##* $q *}" ]; then
-					[[ "$r" == *' -'* ]] && q1="$q" || q1="-$q"
-				elif [ -z "${l1##* -$q *}" ]; then
-					# use.mask work for use.force, but try both vs collisions
-					#r+=" $q"; continue
-					q1="$q"
-				else
-					r1+=" $q"
-					continue
-				fi
-				r+=" $q1"
-				q1="-$q1"
-				$f && [ "$q" != "$6" -a -n "${iuse##* +$q *}" ] && u+=" ${q1#--}"
+		r=" -$q0${q% }"
+		u=
+		$f && {
+			u=" $q0${q// / -}"
+			u="${u%-}"
+			u="${u// --/ }"
+			for j in $u; do
+				[ "${j#-}" = "$6" -o -z "${iuse##* +${j#-} *}" ] && u="${u// $j / }"
 			done
-			[[ "$r" == *' -'* ]] && ap "$p$r${r1:+ #$r1}" "$d/package.use.mask"
-			[ -n "$u" ] && ap "$p$u" "$d/package.use$force"
+			u="${u% }"
+		}
+
+		for p in $(pkg "$i"); do
+			[[ "$r" == *' -'* ]] && ap "$i" "$p" "$r${r1:+ #$r1}" "$d/package.use.mask"
+			[ -n "$u" ] && ap "$i" "$p" "$u" "$d/package.use$force"
 		done
 	done
 done
 }
 
-for i in $(grep -l "^IUSE=.*+\($or_\)" $l); do
+for i in $(grep -l "$re2" $l); do
 	iuse=`grep "^IUSE=" "$i"`
 	iuse=" ${iuse#IUSE=} "
 	iuse1="${iuse// [+~-]/ }"
@@ -174,7 +244,7 @@ for i in $(grep -l "^IUSE=.*+\($or_\)" $l); do
 			[ "$j" = "$v" ] && ! chk6 "$4" "$5" "$v" "$1" "$2" 0 0 && continue
 			r+="-$j "
 		done
-		ap "$p${r% }" "$d/package.use"
+		ap "$i" "$p" "${r% }" "$d/package.use"
 	done
 done
 
@@ -190,6 +260,7 @@ gtk3="$(sl x11-libs/gtk+ 3)"
 qt4="$(sl "dev-qt/qt[a-zA-Z-]*" 4)"
 qt5="$(sl "dev-qt/qt[a-zA-Z-]*" 5)"
 
+generate gles 'gles2 gles gles1' 'opengl' 'gles gles1 opengl egl vaapi' &
 {
 generate common 'opengl egl' 'gles gles1 gles2' 'gles gles1 gles2 egl'
 x1='kernel ssl openssl gnutls nss mhash cryptopp nettle' # enabled
