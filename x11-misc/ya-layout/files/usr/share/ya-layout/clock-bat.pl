4f8050e725c139615a9b5326cb2ebebdb74e0882
mahatma <mahatma@eu.by>
2018-04-17 20:55:59 +0300

    tint2 battery: cosmetic

--- x11-misc/ya-layout/files/usr/share/ya-layout/clock-bat.pl
+++ x11-misc/ya-layout/files/usr/share/ya-layout/clock-bat.pl
@@ -12,18 +12,18 @@ for(glob('/sys/class/power_supply/*/capacity')){
 
 $md=-1;
 while(1){
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-	print sprintf("%02i:%02i\n",$hour,$min),(map{
-		seek($_,0,0);
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime;
+	if($md!=$mday){
+#		use POSIX; $d=strftime('%A %e %B %Y',localtime);
+		$d=localtime; $d=~s/\d\d:\d\d:\d\d *//;
+		print STDERR "\x1b[2J$d\n";
+		$md=$mday;
+	}
+	print sprintf("%02i:%02i\n ",$hour,$min),(map{
 		my $c=readline($_);
+		seek($_,0,0);
 		chomp($c);
 		$c?"$c% ":();
 	}@B),"\n";
-	if($md!=$mday){
-#		use POSIX; $d=strftime('%A %e %B %Y', localtime);
-		$d=localtime; $d=~s/\d\d:\d\d:\d\d *//;
-		print STDERR "\x1b[2J$d";
-		$md=$mday;
-	}
 	sleep(60-$sec);
 }
