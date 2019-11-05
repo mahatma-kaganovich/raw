a6b7743673f53523dcad7c08db06c92c56d60795
mahatma <mahatma@eu.by>
2019-11-05 09:45:06 +0300

    neutral code

--- sys-kernel/genpnprd/files/insmod.c
+++ sys-kernel/genpnprd/files/insmod.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
 	off_t len = lseek(f, 0, 2);
 	lseek(f, 0, 0);
 	for (i=2; i<argc; i++) l+=strlen(argv[i]);
-	char *buf = malloc(len+l), *opt = buf+len;
+	char *buf = malloc(len+l), *opt = &buf[len];
 	for (i=2; i<argc; i++){strcat(opt,argv[i]);strcat(opt," ");}
 	opt[l] = '\0';
 	return read(f,buf,len)==len?init_module(buf,len,opt):1;
