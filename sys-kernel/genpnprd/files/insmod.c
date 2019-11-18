9056992388988c8dd30426bbf5300a2c34f89786
mahatma <mahatma@eu.by>
2019-11-18 14:24:42 +0300

    optimize insmod.c

--- sys-kernel/genpnprd/files/insmod.c
+++ sys-kernel/genpnprd/files/insmod.c
@@ -5,13 +5,21 @@
 extern long init_module(void *, unsigned long, const char *);
 int main(int argc, char *argv[]){
 	if (argc<2) return 1;
-	int f = open(argv[1], 0, 0), i, l = argc+1;
+	int f = open(argv[1], 0, 0);
+	char *buf, *opt;
 	if (f<0) return 1;
 	off_t len = lseek(f, 0, 2);
 	lseek(f, 0, 0);
-	for (i=2; i<argc; i++) l+=strlen(argv[i]);
-	char *buf = malloc(len+l), *opt = &buf[len];
-	for (i=2; i<argc; i++){strcat(opt,argv[i]);strcat(opt," ");}
-	opt[l] = '\0';
+	if (argc<3) {
+		buf = malloc(len);
+		opt = argc==2 ? argv[2] : "";
+	} else {
+		int i, l = argc+1;
+		for (i=2; i<argc; i++) l+=strlen(argv[i]);
+		buf = malloc(len+l);
+		opt = &buf[len];
+		for (i=2; i<argc; i++){strcat(opt,argv[i]);strcat(opt," ");}
+		opt[l] = '\0';
+	}
 	return read(f,buf,len)==len?init_module(buf,len,opt):1;
 }
