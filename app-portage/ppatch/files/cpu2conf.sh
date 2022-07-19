41dc60bebd24f23796c863fbcc3be59e9cc98bb7
mahatma <mahatma@eu.by>
2022-07-19 17:30:00 +0300

    -fvect-cost-model=cheap only ondemand

--- app-portage/ppatch/files/cpu2conf.sh
+++ app-portage/ppatch/files/cpu2conf.sh
@@ -329,7 +329,7 @@ x86_*|i?86)
 	fsmall+=' -fno-ira-loop-pressure'
 	# vs. -O3 -msse
 	# in many cases it also "fast", but keep default / selectable
-	fv=$(_f -fvect-cost-model=cheap -fsimd-cost-model=cheap)
+	_c -Q -O3 --help=optimizers | grep -sq 'fvect-cost-model=.*dynamic$' && fv=$(_f -fvect-cost-model=cheap -fsimd-cost-model=cheap)
 	# rare way, keep only in gcc patch
 #	[ -z "$fv" ] && fv=$(_f -mstackrealign)
 ;;
